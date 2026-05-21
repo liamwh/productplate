0c84c7dc949f474427f20dd8e7f4d5d299b7e135
Rodrigo Dias <me@rgo.pt>
2026-05-21 15:54:15 +0100

    Fix unused imports in autumn.config.ts

--- autumn.config.ts
+++ autumn.config.ts
@@ -1,90 +1,22 @@
-import { feature, product, featureItem } from 'atmn';
+import { product } from 'atmn';
 
 // Features
 
-export const projects = feature({
-	id: 'projects',
-	name: 'Projects',
-	type: 'metered',
-	consumable: false
-});
-
-export const apiCalls = feature({
-	id: 'api_calls',
-	name: 'API Calls',
-	type: 'metered',
-	consumable: true
-});
-
-export const prioritySupport = feature({
-	id: 'priority_support',
-	name: 'Priority Support',
-	type: 'boolean'
-});
-
 // Products
-
 export const free = product({
 	id: 'free',
 	name: 'Free',
-	is_default: true,
-	items: [
-		featureItem({
-			feature_id: projects.id,
-			included_usage: 3
-		}),
-
-		featureItem({
-			feature_id: apiCalls.id,
-			included_usage: 100,
-			interval: 'month'
-		})
-	]
+	items: []
 });
 
 export const pro = product({
 	id: 'pro',
 	name: 'Pro',
-	items: [
-		featureItem({
-			feature_id: projects.id,
-			included_usage: 25
-		}),
-
-		featureItem({
-			feature_id: apiCalls.id,
-			included_usage: 5_000,
-			interval: 'month'
-		}),
-
-		featureItem({
-			feature_id: prioritySupport.id
-		})
-	]
+	items: [],
+	free_trial: {
+		duration: 'undefined',
+		length: undefined,
+		unique_fingerprint: undefined,
+		card_required: true
+	}
 });
-
-export const premium = product({
-	id: 'premium',
-	name: 'Premium',
-	items: [
-		featureItem({
-			feature_id: projects.id,
-			included_usage: 100
-		}),
-
-		featureItem({
-			feature_id: apiCalls.id,
-			included_usage: 25_000,
-			interval: 'month'
-		}),
-
-		featureItem({
-			feature_id: prioritySupport.id
-		})
-	]
-});
-
-export default {
-	features: [projects, apiCalls, prioritySupport],
-	products: [free, pro, premium]
-};
